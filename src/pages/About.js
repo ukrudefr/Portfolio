1bb31face90ddc767b39814ff1d6e37a26de13af
ukrudefr <ukrudefr@gmail.com>
2024-01-09 14:16:13 +0100

    ohne portfolio

--- src/pages/About.js
+++ src/pages/About.js
@@ -98,7 +98,6 @@ export default function About () {
             <h3 className="mb-3 expirience color-grey">Projekten:</h3>
                     <section className="mb-5">
                      <ul>
-                      <li><a href="https://ukrudefr-portfolio.netlify.app">ukrudefr-portfolio.netlify.app</a></li>
                       <li><a href="https://charmeetbeaute68.fr">charmeetbeaute68.fr</a></li>
                       <li><a href="https://tatianas-garden.netlify.app">tatianas-garden.netlify.app</a></li>
                      </ul>
